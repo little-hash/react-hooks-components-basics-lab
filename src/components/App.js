eb29d17d066fe1e96948894ed31775d4dfc87a88
Ezra <ezraraddai166@gmail.com>
2025-01-22 10:22:59 +0300

    firstcommit

--- src/components/App.js
+++ src/components/App.js
@@ -8,23 +8,31 @@ function NavBar() {
   );
 }
 
-function Home() {
-  return (
-    <div id="home">
-      <h1>Home</h1>
+function About(){
+  return(
+    <div id = "about">
+      <h2>About</h2>
     </div>
-  );
+  )
 }
 
-{/* write an <About> component here */}
+function Home() {
+  return(
+    <div id = "home">
+      <h1>Home</h1>
+    </div>
+  )
+}
+
+
 
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home/>
+      <About/>
     </div>
   );
 }
